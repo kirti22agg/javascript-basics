162918da5d951a46071d13c6602e9e385bd1015f
kirti22agg <kirti22agg@gmail.com>
2021-09-13 13:37:10 +0530

    logical operators

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-// comparison operators
-const a=21
-const b=7
-const c=21
-console.log(a>b)
-console.log(a<b)
+// logical operators
+
+const a=2
+const b=3
 console.log(a==b)
-console.log(a==c)
-console.log(a>=c)
+console.log(a!=b)
+console.log('------------------------------')
+// truth table for AND
+
+console.log(false&&false)
+console.log(true&&true)
+console.log(false&&true)
+console.log(true&&false)
 
 console.log('-------------------------------')
+// truth table for OR
 
-const e=11
-const d='11'
+console.log(false||false)
+console.log(true||true)
+console.log(false||true)
+console.log(true||false)
 
-console.log(e==d)
-console.log(e===d)
+console.log('-------------------------------')
+const c=10
+const d=50
 
-const x='kirti'
-const y='KIRTI'
-console.log(x==y)
+const e=35
 
-
-
-
-
-
+console.log(e>c && e<d)
