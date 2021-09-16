9535cc4cbbbd69b373b366a2d1a6a015278dd0de
kirti22agg <kirti22agg@gmail.com>
2021-09-16 19:24:47 +0530

    ES-6 functions

--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 // const paper=document.querySelector('#paper')
 // const scissors=document.querySelector('#scissors')
 
-function logName(Name = "Enter your name"){
-    // console.log("Kirti")
-    console.log(Name)
-}
 
-// logName("Kirti")
-logName()
+// function declaration
 
 function nameage(Name="John Doe", age = 18){
     console.log(Name+ " You are " + age)
 }
-nameage()
+// const add= function(a=0,b=0){
+//     return a+b
+// }
 
-function add(a=0,b=0){
-    const sum=a+b
-    return sum
-}
-const result =add(12,99)
-console.log(result)
+
+const add= (a=0,b=0)=>{
+       return a+b
+    }
+
+    console.log(add(45,98))
+
+
+
