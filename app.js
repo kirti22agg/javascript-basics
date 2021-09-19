14a5411e32a953c51d7433156adbda5599351d6f
kirti22agg <kirti22agg@gmail.com>
2021-09-19 19:46:47 +0530

    misc

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 const balls = document.querySelectorAll('.ball')
 const body= document.querySelector('body')
 
-const arr = [1,2,3,4,5]
-// while(arr.length<15){
-//     arr.push("iteration")
-// }
-// // arr.push(6)
-// console.log(arr)
-// console.log(arr.length)
+balls.forEach( (ball,iteration)=>{
+    const value = iteration*50
+    ball.style = `transform:translateY(${value}px)`
+})
 
-// const added= arr.indexOf(8)
-// const added= arr.includes(8)
-// const added= arr.shift()
-
-// arr.splice(2,2)
-
-
-// console.log(added)
-
-arr.forEach ( (element,iteration) => {
-    
-    console.log(element, iteration)
-} )
-
-// console.log(arr)
-
+let age = 20
+let Name = "Kirti"
+console.log(` Hi ${Name} Your age is ${age}`)
 
 
 
