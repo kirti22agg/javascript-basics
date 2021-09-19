8d842decc129c9ad7ded59929756e77a5cb33f5d
kirti22agg <kirti22agg@gmail.com>
2021-09-19 12:50:54 +0530

    for and while loops

--- app.js
+++ app.js
@@ -1,62 +1,20 @@
-const rock=document.querySelector('#rock')
-const paper=document.querySelector('#paper')
-const scissors=document.querySelector('#scissors')
-const board=document.querySelector('#board')
+let i
 
+for(i=1;i<3;i++) {
+    console.log('iteration ' + i)
+}
 
-
-//  const handler= (action) => {
-//      console.log("You clicked " + action)
-//  }
-
-
-//  rock.addEventListener('click', () => {
-//     handler('rock')
-//  })
-//  paper.addEventListener('click', () => {
-//      handler('paper')
-//  })
-//  scissors.addEventListener('click', () => {
-//      handler('scissors')
-//  })
-
-
-//  const handler= (event) => {
-    
-//     const action = event.target.id
-//     if (action == 'rock') {
-//         board.innerHTML = "rock crushes scissors"
-//     }else if (action == 'paper') {
-//         board.innerHTML = "paper traps rock"
-//     }else if (action == 'scissors') {
-//         board.innerHTML = "scissors cut paper"
-//     }else {
-//         board.innerHTML = "click something"
-//     }
+// const temp =( )=>{
+//     var dontknow= "dontknow"
 // }
-const handler= (event) => {
-const action = event.target.id
+// console.log(dontknow)
 
-switch(action){
-    case "rock":
-        board.innerHTML = "rock crushes scissors"
-        break
-    case "paper" :
-        board.innerHTML = "paper traps rock"
-        break
-    case "scissors":
-        board.innerHTML = "scissors cut paper"
-        break
-    default:
-        board.innerHTML = "click something"
+let iteration=0
+while(iteration<10){
+    iteration++
+    console.log(iteration)
+}
 
-
-} }
-
-
-rock.addEventListener('click', handler)
-paper.addEventListener('click',  handler)
-scissors.addEventListener('click', handler)
 
 
 
