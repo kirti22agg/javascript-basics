92f3ac85bb7c4392d35a8db1bbfb9c12fd4ead46
kirti22agg <kirti22agg@gmail.com>
2021-09-24 18:54:15 +0530

    async and await

--- app.js
+++ app.js
@@ -18,7 +18,33 @@ console.log(attendConcert)
 // attendConcert.catch((error)=> console.log(error))
 // attendConcert.then((data)=> console.log(data))
 
-attendConcert.then((data)=> console.log(data)).catch((error)=> console.log(error))
+// attendConcert.then((data)=> console.log(data)).catch((error)=> console.log(error))
+
+async function afunc() {
+    try{
+        let result = await attendConcert
+        console.log(result)
+    }
+    catch(err) {
+        console.log(err)
+    }
+
+    
+    
+}
+
+afunc()
+
+// arrow notation (try using this as much as possible)
+
+// const afunc = async() =>{
+//     let result = await attendConcert()
+//     return result
+
+// }
+
+console.log("HI.I WON'T WAIT FOR ANYONE!")
+
 
 
 
